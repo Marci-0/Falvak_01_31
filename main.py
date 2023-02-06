76a09e194c279c9ea16500683324ca145b9f7021
KirillaJozsef <10.bkirillajozsef.wattay@gmail.com>
2023-02-06 12:12:18 +0100

    Kirilla József

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ if __name__ == '__main__':
     print("na ez csak fetch, pull vagy clone esetén fog látszódni")
     print("órai munka: Utopia")
     print("Órai munk:TA")
+    print("Órai munka:KJ(JózsiKJ)")
