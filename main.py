66e8b8ef20b12d61ca615e5f37f8065445bc6c0d
d0mek007 <10.bszabodominik.wattay@gmail.com>
2023-02-06 12:03:45 +0100

    Szabó Dominik

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 if __name__ == '__main__':
-    print("adalék")
-    print("na ez csak fetch, pull vagy clone esetén fog látszódni")
-    print("órai munka: Utopia")
-    print("Órai munk:TA")
+    print("Órai munka:SZD")
